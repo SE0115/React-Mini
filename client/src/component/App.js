ec6001d0c907e8d3b2a51fbb134f5f8ad405091e
sieun <sieun0115@ajou.ac.kr>
2021-12-23 21:38:07 +0900

    feat: Manage permission

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import Header from "../route/Header";
 import Footer from "../route/Footer";
 import "bootstrap/dist/css/bootstrap.css";
@@ -11,10 +11,73 @@ import Product from "../component/product/Product";
 import Cart from "../component/cart/Cart";
 import History from "../component/history/History";
 import Naver from "./naver/Naver";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import cookie from "react-cookies";
+import axios from "axios";
 
 function App() {
   const [userId, setUserId] = useState("");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if ("/register" !== location.pathname) {
+      handleCheckSession();
+    }
+  }, [location.pathname]);
+
+  const handleCheckSession = async () => {
+    const tId = cookie.load("token_id");
+    const tName = cookie.load("token_name");
+    const tPwd = cookie.load("user_password");
+
+    if (tId && tName) {
+      axios
+        .post("/api/user?type=sessionCheck", {
+          token_id: tId,
+          token_name: tName,
+        })
+        .then((res) => {
+          // 유저 아이디 세팅
+          setUserId(res.data.decrypt_id.user_email);
+          // 쿠키의 password 검증
+          if (tPwd) {
+            axios
+              .post("/api/user?type=sessionSignin", {
+                user_email: res.data.decrypt_id.user_email,
+                user_password: tPwd,
+              })
+              .then((res) => {
+                if (!res.data[0].user_email) {
+                  // 로그인 상태 해제
+                  handleNotLogin();
+                }
+              })
+              .catch((err) => {});
+          } else {
+            // 로그인 상태 해제
+            handleNotLogin();
+          }
+        });
+    } else {
+      // 로그인 상태 해제
+      handleNotLogin();
+    }
+  };
+
+  const handleNotLogin = () => {
+    if (window.location.hash !== "nocookie") {
+      handleRemoveCookie();
+    }
+    setTimeout(() => {
+      window.location.href = "/login/#nocookie";
+    }, 1000);
+  };
+  const handleRemoveCookie = () => {
+    cookie.remove("token_id", { path: "/" });
+    cookie.remove("token_name", { path: "/" });
+    cookie.remove("user_password", { path: "/" });
+  };
 
   return (
     <div className="App">
