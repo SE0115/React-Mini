404c8c42b989e87c44ffaa4b800800425a7c0c8a
sieun <sieun0115@ajou.ac.kr>
2021-12-23 16:52:43 +0900

    Get category list

--- client/src/component/product/Product.js
+++ client/src/component/product/Product.js
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from 'axios'
 
-const Product = (props) => {
+function Product() {
+  const [category, setCategory] = useState({
+    c1: [],
+    c2: [],
+    c3: [],
+    c4: []
+  })
+  const [selected, setSelected] = useState({
+    c1: '',
+    c2: '',
+    c3: '',
+    c4: ''
+  })
+  const onChange = (event) => {
+    const { name, value } = event.target
+    setSelected({
+      ...selected,
+      [name]: value
+    })
+  }
+  
+  const category1 = () => {
+    axios
+      .post('/api/product?type=category', { num: 1 })
+      .then((res) => {
+        setCategory({
+          ...category,
+          c1: res.data.json.map(x => x.category1)
+        })
+      })
+  }
+  const category2 = (c1) => {
+    axios
+      .post('/api/product?type=category', {
+        num: 2,
+        category1: c1
+      })
+      .then((res) => {
+        // setCategory({
+        //   ...category,
+        //   c2: res.data.json.map(x => x.category2)
+        // })
+        return res.data.json.map(x => x.category2)
+      })
+  }
+
+  useEffect(() => {
+    console.log(selected)
+    console.log(selected.c1)
+    category2()
+  }, [selected])
+  
   return (
     <div>
       <h2 style={{ textAlign: "center", padding: "10px" }}>상품</h2>
+      <div>
+        <select name="c1" onChange={onChange} onClick={category1}>
+          <option>카테고리 1</option>
+          {
+            category.c1.map((option, idx) => (
+              <option value={option} key={idx}>{option}</option>
+            )) 
+          }
+        </select>
+        <select name="c2" >
+          <option>카테고리 2</option>
+          {/* {
+            category.c1.map(option => (
+              <option value={option}>{option}</option>
+            )) 
+          } */}
+        </select>
+        <select name="c3" >
+          <option>카테고리 3</option>
+          {/* {
+            category.c1.map(option => (
+              <option value={option}>{option}</option>
+            )) 
+          } */}
+        </select>
+        <select name="c4" >
+          <option>카테고리 4</option>
+          {/* {
+            category.c1.map(option => (
+              <option value={option}>{option}</option>
+            )) 
+          } */}
+        </select>
+        <button>검색</button>
+      </div>
     </div>
   );
 };
